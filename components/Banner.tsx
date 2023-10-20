ee732f1e2b3c834554569f6178fcd7737146658f
Arthur Dias <dias.arthurr@gmail.com>
2023-10-20 15:03:06 -0300

    Fixed banner label error

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,7 +1,7 @@
 import { Banner as BannerType } from '@/types'
 
 interface BannerProps {
-  data: BannerType
+  data?: BannerType
 }
 
 const Banner: React.FC<BannerProps> = ({ data }) => {
@@ -11,9 +11,11 @@ const Banner: React.FC<BannerProps> = ({ data }) => {
         style={{ backgroundImage: `url(${data?.imageUrl})` }}
         className='rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover'>
         <div className='h-full w-full flex flex-col justify-center items-center text-center gap-y-8'>
-          <div className='font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs'>
-            {data.label}
-          </div>
+          {data?.label ? (
+            <div className='font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs'>
+              {data?.label}
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
